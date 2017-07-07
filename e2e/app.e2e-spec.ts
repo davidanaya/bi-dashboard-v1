a1f0bf32886c1543d4362fbe35a1235946bcc1b6
david <danaya@gmail.com>
2017-07-07 15:05:49 +0200

    fix e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('my-angular-cli-seed App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toEqual('Welcome to my-angular-cli-seed!!');
   });
 });
